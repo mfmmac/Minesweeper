9e1089c156652340ca8e46f6bd6d9ae323e7864c
Shari <shares_322@yahoo.com>
2020-08-13 21:16:09 -0400

    Adding project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GroupDMinefieldMidterm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
